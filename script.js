a50ddce9a33bb90df95d309d3c847ae1abe6b7c4
Wikynner <Wikynner@gmail.com>
2024-06-11 16:49:31 -0400

    concatenar com computado

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let app = new Vue({
     el:'#app',
     data:{
-       conta:'',
-       aviso:'',
-       resultado:'',
-       timer:null
+     primeiroNome:'',
+     segundoNome:''
     },
     methods: {
-        fazerConta:function(){
-            this.aviso = '';
-
-            this.resultado = eval(this.conta);
-        }
+    
     },
     watch:{
-        conta:function(){
-            this.aviso ='Digitando...'
-            if(this.timer != null){
-                clearTimeout(this.timer);
-            }
-            this.timer = setTimeout(this.fazerConta,1000);
+       
+    },
+    computed:{
+        nomeCompleto:function(){
+
+            return this.primeiroNome+' '+this.segundoNome;
         }
+       
     }
 
    });
