bbf4959316396da78ae111dd9e46abedae4c505f
Wikynner <Wikynner@gmail.com>
2024-06-13 15:37:47 -0400

    ex Semaforo

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
+let bola = {
+  props:['cor'],
+  template:`
+  <div class="bola" v-bind:class="cor">
+  </div>
+  `
+};
+Vue.component('sinal',{
+    props:['status'],
+    components:{
+    bola
+},
+ template:`
+    <div class="sinal">
+        <template v-if="status == 'red'">
+        <bola cor="red"/>
+        <bola />
+        <bola />
+        </template>
+        <template v-else-if="status == 'yellow'">
+      
+        <bola />
+        <bola cor="yellow"/>
+        <bola />
+        </template>
+        <template v-if="status == 'green'">
+        
+        <bola />
+        <bola />
+        <bola cor="green" />
+        </template>
+    </div>
+ `
+
+});
+
 let app = new Vue({
-    el:'#app',
-    data:{
-     primeiroNome:'',
-     segundoNome:''
+  el:'#app',
+  data:{
+    sinalStatus:'red'
+  },
+  methods: {
+    liberar:function(){
+      this.sinalStatus = 'green';
+
     },
-    methods: {
     
-    },
-    watch:{
-       
-    },
-    computed:{
-        nomeCompleto:function(){
+    fechar:function(){
+      this.sinalStatus = 'yellow';
 
-            return this.primeiroNome+' '+this.segundoNome;
-        }
-       
+      setTimeout(this.fecharSinal,1000)
+   
+
+    },
+    fecharSinal:function(){
+      this.sinalStatus = 'red';
+
     }
-
-   });
+    }
+});
